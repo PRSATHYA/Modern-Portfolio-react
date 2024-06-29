4f5dc8565679cdb351708833236e182cc9cf3c78
SATHYA POOMARI <124229960+PRSATHYA@users.noreply.github.com>
2024-06-29 14:29:34 +0530

    Update Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -53,7 +53,7 @@ function Hero() {
           </a>
         </span>
         <p className={styles.description}>
-          With a passion for developing modern React web apps & UI/UX Designer for commercial
+          With a passion for developing modern React web apps & UI/UX Design for commercial
           businesses.
         </p>
         <a href={CV} download>
